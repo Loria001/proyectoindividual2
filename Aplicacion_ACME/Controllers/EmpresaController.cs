c645b728ea28840e80cf7750a4593d01e245ecc2
jerem <jdormond@edu.upolitecnica.cr>
2024-04-19 23:01:52 -0600

    Agregar archivos de proyecto.

---
+++ Aplicacion_ACME/Controllers/EmpresaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.ACME;
+using Services.ACME;
+
+namespace Aplicacion_ACME.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            EmpresaService empresaService = new EmpresaService();
+            List<EmpresaEntidad>? listaEmpresaEntidad;
+
+            listaEmpresaEntidad = empresaService.Listar();
+
+            return View(listaEmpresaEntidad);
+        }
+    }
+}
